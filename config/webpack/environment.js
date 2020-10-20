76209ab27e120edac1c414b5909d83927cd4eb55
Yuki Nakato <crystal.icicle@gmail.com>
2020-10-20 23:34:37 +0900

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,7 +3,8 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery'
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
   })
 )
 
